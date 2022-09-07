7d741586dcbc4b6979bc245e7f9ce359647199d9
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-09-07 18:02:10 +0200

    Update TaxCalculationViewModel.cs

--- Mvvm_Design_pattern_tutorial/TaxCalculationViewModel.cs
+++ Mvvm_Design_pattern_tutorial/TaxCalculationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 namespace Mvvm_Design_pattern_tutorial
 {
     //https://www.clariontech.com/blog/wpf-with-mvvm-easily-separate-ui-and-business-logic
-    public class TaxCalculationViewModel :INotifyPropertyChanged
+    public class TaxCalculationViewModel : INotifyPropertyChanged
     {
         private TaxCalculationModel _TaxCalculationModel;
         public TaxCalculationViewModel(TaxCalculationModel pTaxCalculationModel)
@@ -16,7 +17,35 @@ namespace Mvvm_Design_pattern_tutorial
         }
         public decimal TotalIncome
         {
-            
+           get { return _TaxCalculationModel.TotalIncome; }
+            set { _TaxCalculationModel.TotalIncome = value; Changed("TotalIncome"); TaxCalculation(); }
         }
+        public decimal ExemptedIncome
+        {
+            get { return _TaxCalculationModel.ExemptedIncome; }
+            set { _TaxCalculationModel.ExemptedIncome = value; Changed("ExemptedIncome"); TaxCalculation(); }
+        }
+        public decimal TaxableIncome
+        {
+            get { return _TaxCalculationModel.TaxableIncome; }
+            set { _TaxCalculationModel.TaxableIncome = value; Changed("TaxableIncome"); }
+        }
+        public int PercentageOfTax
+        {
+            get { return _TaxCalculationModel.PercentageOfTax; }
+            set { _TaxCalculationModel.PercentageOfTax = value; Changed("PercentageOfTax"); TaxCalculation(); }
+        }
+        public decimal TaxOnIncome
+        {
+            get { return _TaxCalculationModel.TaxOnIncome; }
+            set {    _TaxCalculationModel.TaxOnIncome = value; Changed("TaxOnIncome"); }
+        }
+        private void TaxCalculation()
+        {
+            TaxableIncome = TotalIncome - ExemptedIncome;
+            if(TaxableIncome > 0 && PercentageOfTax > 0)
+                TaxOnIncome = (PercentageOfTax * TaxableIncome) / 100;
+        }
+
     }
 }
