50f3d4bc5f13149b7f84ac4282ea6e43b5eed630
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-09-07 19:38:01 +0200

    Project finished

--- Mvvm_Design_pattern_tutorial/TaxCalculationViewModel.cs
+++ Mvvm_Design_pattern_tutorial/TaxCalculationViewModel.cs
@@ -2,7 +2,6 @@
 
 namespace Mvvm_Design_pattern_tutorial
 {
-    //https://www.clariontech.com/blog/wpf-with-mvvm-easily-separate-ui-and-business-logic
     public class TaxCalculationViewModel : INotifyPropertyChanged
     {
         private TaxCalculationModel _TaxCalculationModel;
