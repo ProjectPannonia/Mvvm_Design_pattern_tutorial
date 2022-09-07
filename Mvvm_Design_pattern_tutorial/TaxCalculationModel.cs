b3feb1d605b12840e2f08f048e55c68642352f87
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-09-07 10:04:07 +0200

    Add project files.

---
+++ Mvvm_Design_pattern_tutorial/TaxCalculationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Mvvm_Design_pattern_tutorial
+{
+    public class TaxCalculationModel
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal ExemptedIncome { get; set; }
+        public decimal TaxableIncome { get; set; }
+        public int PercentageOfTax { get; set; }
+        public decimal TaxOnIncome { get; set; }
+
+    }
+}
---
+++ Mvvm_Design_pattern_tutorial/TaxCalculationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvvm_Design_pattern_tutorial
+{
+    //https://www.clariontech.com/blog/wpf-with-mvvm-easily-separate-ui-and-business-logic
+    public class TaxCalculationViewModel :INotifyPropertyChanged
+    {
+        private TaxCalculationModel _TaxCalculationModel;
+        public TaxCalculationViewModel(TaxCalculationModel pTaxCalculationModel)
+        {
+            _TaxCalculationModel = pTaxCalculationModel;
+        }
+        public decimal TotalIncome
+        {
+            
+        }
+    }
+}
